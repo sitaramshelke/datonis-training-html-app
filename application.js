3ebf8f587e91a78262cfa2c4597a246e8f1f4f0c
sitaramshelke <sitaramshelke@gmail.com>
2018-10-20 22:14:36 +0530

    Checkpoint-2

--- application.js
+++ application.js
@@ -10,6 +10,5 @@ app.use(bodyParser.json()) //Set json parser for server handle
 app.use(express.static("./public")) //Set server handle to server all file from public directory
 
 app.get("/things", thing.get_things); //Define a GET url and map it to a handler function
-app.post("/get_thing_data", thing.get_thing_data); //Define a POST url and map it to a handler function
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`)); //Bind the server to a port and start listening
